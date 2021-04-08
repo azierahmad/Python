6abf43d23813478c8e8160dd1bf36685f7dcaa7a
Azier <a.mohammad44@protonmail.com>
2021-04-08 22:21:34 +0300

    first commit

---
+++ python_fundamentals/For Loops Basic I/ForLoopsBasicOne.py
@@ -0,0 +1,47 @@
+# 1- Basic - I will Print all integers from 0 to 200.
+
+# for i in range(0,201,1):
+#     print(i)
+# print("------------")
+
+# 2- Multiples of Five - I will Print all the multiples of 5 from 5 to 500.
+
+# for z in range(0,501,5):
+#     print(z)
+# print("------------")
+
+# 3- Counting, the Azier Ahmad Way - I will Print integers 1 to 100. and If divisible by 5, print "Azier" instead. If divisible by 10, print "Azier Ahmad".
+
+# for m in range(1,101):
+#     if m%10 == 0:
+#         print(m,"Azier Ahmad")
+#     elif m%5 == 0:
+#         print(m, "Azier")
+#     else:
+#         print(m)
+# print("------------")
+
+# 4- i will Add odd integers from 0 to 500,000, and print the final sum.
+
+# sum = 0
+# for r in range(1,500000,2):
+#     sum +=r
+# print(sum)
+# print("------------")
+
+#5- Countdown by Fours - I will Print positive numbers starting at 2020, counting down by fours.
+
+# for q in range (2020, 0, -4):
+#     print(q)
+# print("------------")
+
+#6- Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+# lowNum = 2
+# highNum = 9
+# mult = 3
+#
+# for p in range(lowNum, highNum+1):
+#     if p%mult == 0:
+#         print(p)
+# print("------------")
